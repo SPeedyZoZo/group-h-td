5729f63f1e6bb6539700accec44d492782748052
itsjrf <ec21581@qmul.ac.uk>
2023-10-24 12:19:17 +0100

    shooting and camera angle tweak

--- prototypeGD/Assets/Turret.cs
+++ prototypeGD/Assets/Turret.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
 
-    private Transform target;
+    private Transform target; // change back to public if you want to test/bug fix 
+
+
+
+    [Header("Attributes")]
+
     public float range;
+    public float fireRate = 1f;
+    public float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
 
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
     public float turnSpeed = 15;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +64,27 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f); // Only rotate Y axis not all
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f/ fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+
+    }
+
+    void Shoot()
+    {
+        // Debug.Log("Shoot");
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if  (bullet != null)
+            bullet.Seek(target);
+
+
 
     }
 
