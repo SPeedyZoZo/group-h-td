b872824fe00dc5ab2f9a0fb57bcfce7ab43f9dec
Zaid Chughtai <71060384+SPeedyZoZo@users.noreply.github.com>
2023-11-28 03:01:32 +0300

    Added graphics dropdown menu

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
+using TMPro;
+using System.Linq;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -10,39 +12,42 @@ public class SettingsManager : MonoBehaviour
     public Button backButton;
     public AudioSource audioSource;
     public AudioClip volumeChangeClip;
+    public TMP_Dropdown graphicsQualityDropdown;
 
     private Coroutine playSoundCoroutine;
 
     void Start()
     {
+        // Set up graphics quality dropdown
+        graphicsQualityDropdown.AddOptions(QualitySettings.names.ToList());
+        int savedQualityIndex = PlayerPrefs.GetInt("GraphicsQuality", QualitySettings.GetQualityLevel());
+        graphicsQualityDropdown.value = savedQualityIndex;
+        QualitySettings.SetQualityLevel(savedQualityIndex);
+        graphicsQualityDropdown.onValueChanged.AddListener(SetGraphicsQuality);
+
         // Load previously saved volume
         volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1.0f); // Default to 100% volume if not set
-
-        // Attach a listener to the volume slider to handle the volume change
         volumeSlider.onValueChanged.AddListener(delegate { OnVolumeChange(); });
 
-        // Initialize the EventTrigger and add PointerDown and PointerUp event listeners
+        // Initialize EventTrigger for volume slider
         EventTrigger trigger = volumeSlider.gameObject.AddComponent<EventTrigger>();
-
         var pointerDownEntry = new EventTrigger.Entry { eventID = EventTriggerType.PointerDown };
         pointerDownEntry.callback.AddListener((data) => { OnPointerDown(); });
         trigger.triggers.Add(pointerDownEntry);
-
         var pointerUpEntry = new EventTrigger.Entry { eventID = EventTriggerType.PointerUp };
         pointerUpEntry.callback.AddListener((data) => { OnPointerUp(); });
         trigger.triggers.Add(pointerUpEntry);
 
+        // Setup button listeners
         saveButton.onClick.AddListener(SaveSettings);
         backButton.onClick.AddListener(BackToMainMenu);
     }
 
-    // This method will be called whenever the volume slider's value changes
     public void OnVolumeChange()
     {
         AudioListener.volume = volumeSlider.value;
     }
 
-    // Called when the user starts dragging the slider or clicks on it
     private void OnPointerDown()
     {
         if (playSoundCoroutine != null)
@@ -52,7 +57,6 @@ public class SettingsManager : MonoBehaviour
         playSoundCoroutine = StartCoroutine(PlaySoundWhileInteracting());
     }
 
-    // Called when the user stops dragging the slider or releases the click
     private void OnPointerUp()
     {
         if (playSoundCoroutine != null)
@@ -63,12 +67,10 @@ public class SettingsManager : MonoBehaviour
 
     private IEnumerator PlaySoundWhileInteracting()
     {
-        // Play the initial sound immediately upon interaction
         if (audioSource != null && volumeChangeClip != null)
         {
             audioSource.PlayOneShot(volumeChangeClip);
         }
-        // Continue playing the sound every 0.5 seconds while interacting
         while (true)
         {
             yield return new WaitForSeconds(0.5f);
@@ -79,16 +81,20 @@ public class SettingsManager : MonoBehaviour
         }
     }
 
-    // This method saves the volume setting
     public void SaveSettings()
     {
         PlayerPrefs.SetFloat("Volume", volumeSlider.value);
+        PlayerPrefs.SetInt("GraphicsQuality", graphicsQualityDropdown.value);
         PlayerPrefs.Save();
     }
 
-    // Call this method to go back to the Main Menu
     private void BackToMainMenu()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+    }
+
+    public void SetGraphicsQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 }
