5ed40a231056fca88c5241326bbb732190035d1a
Ben Price <ben.jules.price@gmail.com>
2024-01-05 01:49:31 +0000

    feat: multiple spawns

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,8 +15,8 @@ public class Enemy : MonoBehaviour
 
     public AudioClip deathSound;
 
-    private Transform target;
-    private int wavepointIndex = 0;
+    private Transform[] path;
+    private int pathIndex = -1;
 
     public delegate void DealDamage(float damage);
     public static event DealDamage onDealDamage;
@@ -26,39 +26,36 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
-        target = Waypoints.points[0];
         health = maxHealth;
+    }
+
+    public void Seek(Transform[] path)
+    {
+        this.path = path;
+        pathIndex = 0;
     }
 
     void Update() 
     {
-        Vector3 direction = target.position - transform.position;
-        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position)<= distanceToTurn) 
+        if (pathIndex != -1)
         {
-            GetNextWaypoint();
+            Transform target = path[pathIndex];
+            Vector3 direction = target.position - transform.position;
+            transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+            if (Vector3.Distance(transform.position, target.position) <= distanceToTurn)
+            {
+                pathIndex++;
+                if (pathIndex >= path.Length)
+                {
+                    onDealDamage(damage);
+                    Destroy(gameObject);
+                }
+            }
         }
 
         Color color = Color.Lerp(minHealthColor, maxHealthColor, health / maxHealth);
         gameObject.GetComponent<Renderer>().material.color = color;
-    }
-
-    void GetNextWaypoint() 
-    {
-        if (wavepointIndex >= Waypoints.points.Length - 1) 
-        {
-            EndPath();
-            return;
-        }
-        wavepointIndex++;
-        target = Waypoints.points[wavepointIndex];
-    }
-
-    void EndPath()
-    {
-        onDealDamage(damage);
-        Destroy(gameObject);
     }
 
     public void TakeDamage(float damage)
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,7 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [System.Serializable]
+    public struct Wave
+    {
+        public float interval;
+        public Enemy[] enemies;
+    }
+
+    public Wave[] waves;
+    private int waveIndex;
+
     private Transform[] path;
 
     private void Awake()
@@ -9,5 +20,25 @@ public class Spawner : MonoBehaviour
         path = new Transform[transform.childCount];
         for (int i = 0; i < path.Length; i++)
             path[i] = transform.GetChild(i);
+    }
+
+    public int GetNextWaveEnemies()
+    {
+        return waveIndex >= waves.Length
+            ? 0
+            : waves[waveIndex].enemies.Length;
+    }
+
+    public IEnumerator SpawnNextWave()
+    {
+        if (waveIndex >= waves.Length)
+            yield break;
+
+        Wave wave = waves[waveIndex++];
+        foreach (Enemy enemy in wave.enemies)
+        {
+            Instantiate(enemy, transform.position, transform.rotation).Seek(path);
+            yield return new WaitForSeconds(wave.interval);
+        }
     }
 }
